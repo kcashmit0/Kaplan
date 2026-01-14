532b7674941fce35c564a01b8a4a636f9460f374
kcashmit0 <kcashmit0@gmsil.com>
2026-01-15 01:59:38 +1030

    add data loading and analysis to practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,11 @@
-print("hello world")
+import pandas as pd
+
+#load the dataset
+df = pd.read_csv("Sample Dataset.csv")
+
+print(df.head()) # display the first few rows of the dataframe
+print(df.shape) # display the shape of the dataframe
+print(df.info()) # display the summary information of the dataframe
+print(df['Price'].describe()) # display the statistical summary of the 'Price' column
+print(df.isnull().sum()) # display the count of missing values in each column  
+
