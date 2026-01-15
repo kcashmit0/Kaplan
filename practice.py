b169650d5f70ca3530b873c8aae6e143f7ce0618
kcashmit0 <kcashmit0@gmsil.com>
2026-01-15 14:13:47 +1030

    add data processing and feature selection in practice.py and create practice.ipynb

--- practice.py
+++ practice.py
@@ -9,3 +9,15 @@ print(df.info()) # display the summary information of the dataframe
 print(df['Price'].describe()) # display the statistical summary of the 'Price' column
 print(df.isnull().sum()) # display the count of missing values in each column  
 
+# Select only the columns we want
+features = ['Brand', 'Year', 'Model', 'Car/Suv', 'UsedOrNew', 
+            'Transmission', 'Engine', 'DriveType', 'FuelType', 
+            'FuelConsumption', 'Kilometres', 'Location', 'CylindersinEngine']
+
+target = 'Price'
+
+df_model = df[features + [target]].copy()# Create a smaller dataframe with just these columns
+print(df_model.head())
+print(df_model.isnull().sum())
+
+
